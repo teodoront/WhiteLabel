1e4d3aeb9704f68f97f67d9efa29cab77c8d6800
orlando <orlando.neto@maximatech.com.br>
2021-10-27 23:50:23 -0300

    Commit_9

--- app/src/main/java/br/com/orlandoneto/whitelabel/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/orlandoneto/whitelabel/ui/addproduct/AddProductFragment.kt
@@ -1,15 +1,27 @@
 package br.com.orlandoneto.whitelabel.ui.addproduct
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.orlandoneto.whitelabel.databinding.AddProductFragmentBinding
+import br.com.orlandoneto.whitelabel.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: AddProductFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private var imageUri: Uri? = null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){
+        imageUri = it
+        binding.imageProduct.setImageURI(it)
+
+    }
 
     private lateinit var viewModel: AddProductViewModel
 
@@ -22,5 +34,36 @@ class AddProductFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
-    /*Dando vida ao formulário e seleção de imagem*/
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+
+    private fun setListeners(){
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+
+        }
+
+        binding.inputPrice.run {
+
+            addTextChangedListener(CurrencyTextWatcher(this))
+
+        }
+    }
+
+    private fun chooseImage(){
+        getContent.launch("image/*")
+    }
+/*Implementando o ViewModel e Validando o Formulário
+
+ */
+
 }
